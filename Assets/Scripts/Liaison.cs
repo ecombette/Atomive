aefdb445d3b41323c2b0e7fd4ea50c2f74979edf
IvanDesClaviers <ivan.alt@outlook.fr>
2018-02-16 17:18:47 -0500

    Vrai update Atom et Liaison

---
+++ Assets/Scripts/Liaison.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Liaison : MonoBehaviour {
+
+    private Atom atome1;
+    private Atom atome2;
+
+    // Should be called by the Controller to make a Bound between 2 atoms
+    public void BoundAtoms(Atom a, Atom b)
+    {
+        atome1 = a;
+        atome2 = b;
+
+        atome1.Bound(this);
+        atome2.Bound(this);
+    }
+
+    //Ensure to free lhe list of Bounds of each atom
+    void OnDestroy()
+    {
+        if (atome1 != null) atome1.UnBound(this);
+        if (atome2 != null) atome2.UnBound(this);
+    }
+
+    #region MoleculeManager    
+    private bool marked = false;
+
+    public bool isMarked() { return marked; }
+    public void Mark() { marked = true; }
+    public void resetMark() { marked = false; }
+
+    #endregion
+}
