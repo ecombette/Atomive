db27a6902270b6a7715939398fe317d0eecf2817
IvanDesClaviers <ivan.alt@outlook.fr>
2018-02-16 15:29:24 -0500

    'walla'

---
+++ Atom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atom : MonoBehaviour {
+
+    public int nbLiaisonMax;
+    private int nbLiaisonRestante;
+
+    /**
+     * Est appellé dés qu'une liaison est accrochée à l'atome
+     */
+    void Lier()
+    {
+        if(nbLiaisonRestante > 0)
+        {
+            nbLiaisonRestante--;
+        }
+        else
+        {
+            //Pas possible les amis
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        nbLiaisonRestante = nbLiaisonMax;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
