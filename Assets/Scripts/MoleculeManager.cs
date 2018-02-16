380ae5e9f5b815ebba9ba1cdc673cd903d5131c8
ecombette <elise.combette@gmail.com>
2018-02-16 16:13:00 -0500

    Remove useless scripts and initialize MoleculeMnanager

--- Assets/Scripts/MoleculeManager.cs
+++ Assets/Scripts/MoleculeManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Carbon : Atom {
+public class MoleculeManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
